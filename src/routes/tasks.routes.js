f035998ece7039f7da1e3c97872cf2b703cd530a
Carlos Urdaneta <curdanet@hotmail.com>
2022-12-21 01:23:05 -0300

    verifying router / before paths

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -6,11 +6,11 @@ router.get('/', sayOk);
 
 router.get('/api/tasks', getAllTasks);
 
-router.get('api/tasks/:id', getTask);
+router.get('/api/tasks/:id', getTask);
 
-router.post('api/tasks', createTask);
+router.post('/api/tasks', createTask);
 
-router.delete('api/tasks/:id', deleteTask);
+router.delete('/api/tasks/:id', deleteTask);
 
-router.put('api/tasks/:id', updateTask);
+router.put('/api/tasks/:id', updateTask);
 module.exports = router
